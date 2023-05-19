1544c4612ef70be7796348360fe119114b989d36
Rúbem Vieira <rubemvn17@gmail.com>
2023-05-19 09:57:58 -0300

    Adicionando arquivo das aulas 01 - 03

---
+++ FUNDAMENTOS/01_modulos_internos/index.js
@@ -0,0 +1,5 @@
+const modulo = require("./meu_modulo");
+const soma = modulo.soma
+
+
+console.log(soma(2, 3))
---
+++ FUNDAMENTOS/02_import_export/index.mjs
@@ -0,0 +1,4 @@
+import soma from "./meu_modulos.mjs"
+
+console.log(soma(8,4));
+console.log(soma(82,40));
