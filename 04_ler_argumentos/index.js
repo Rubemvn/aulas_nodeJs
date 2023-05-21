303d2e4ef8296884f86872ccabc61da6b5e7e288
Rúbem Vieira <rubemvn17@gmail.com>
2023-05-20 23:13:10 -0300

    Arquivos da aula 04 adicionados

---
+++ 04_ler_argumentos/index.js
@@ -0,0 +1,12 @@
+
+
+// console.log(process.argv);
+
+const args = process.argv.slice(2);
+
+console.log(args)
+
+const nome = args[0].split("=")[1]
+const idade = args[1].split("=")[1]
+
+console.log(`O nome dele é ${nome} e ele tem ${idade} anos.`)
