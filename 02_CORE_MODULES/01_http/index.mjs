c0f530dfa9f510d928b429d4a0dddfb5ad123c37
Rúbem Vieira <rubemvn17@gmail.com>
2023-05-23 17:47:31 -0300

    Adicionando primeiros arquivos do assunto de CORE MODULES

---
+++ 02_CORE_MODULES/01_http/index.mjs
@@ -0,0 +1,13 @@
+import http from 'http';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.write('Oi Http');
+    res.end();
+})
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+})
+
